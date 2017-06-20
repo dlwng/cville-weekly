1023bcc44825de8dcb1cb243cb6299927c8f4f1c
Margaret Chen <Margaret@margarets-mbp.lan>
2017-06-20 15:11:05 -0400

    created footer and made it responsive on all screens

--- footer.php
+++ footer.php
@@ -14,17 +14,38 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cville-weekly' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cville-weekly' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cville-weekly' ), 'cville-weekly', '<a href="https://automattic.com/">Ben, Margaret, Dorothy, and Jocelyn</a>' );
-			?>
-		</div><!-- .site-info -->
+		<div class="contact-text">
+			<div class = "grid__col--1-of-3 grid__col">
+				<p>C-VILLE Weekly</p>
+			</div><div class = "grid__col--1-of-3 grid__col">
+				<p>308 East Main Street</p>
+			</div><div class = "grid__col--1-of-3 grid__col">
+				<p>Charlottesville, VA 22902</p>
+			</div>
+
+			<div class = "grid__col--1-of-5 grid__col">
+				<p>CONTACT US:</p>
+			</div><div class = "grid__col--1-of-5 grid__col">
+				<p>news@c-ville.com</p>
+			</div><div class = "grid__col--1-of-5 grid__col">
+				<p>arts@c-ville.com</p>
+			</div><div class = "grid__col--1-of-5 grid__col">
+				<p>editor@c-ville.com</p>
+			</div><div class = "grid__col--1-of-5 grid__col">
+				<p>eatdrink@c-ville.com</p>
+			</div>
+		</div>
+
+		<div class="mobile-contact-text">
+			<div class = "grid__col--1-of-2 grid__col">
+				<img src="<?php echo home_url(); ?>/wp-content/themes/cville-weekly/images/transp-logo.png" id="footer-logo">
+				<p id="business-address">308 East Main St, <br> Charlottesville,  22902</p>
+			</div><div class = "grid__col--1-of-2 grid__col right-mobile-column">
+				<p id="contact-us">CONTACT US</p>
+				<p>news@c-ville.com <br> arts@c-ville.com <br> editor@c-ville.com <br> eatdrink@c-ville.com</p>
+			</div>
+		</div>
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
