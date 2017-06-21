189a2e756e0a2c593297b3885cdf0184678643f2
Margaret Chen <Margaret@margarets-mbp.lan>
2017-06-21 16:01:39 -0400

    created main article template with proper positioning and hover effects

--- index.php
+++ index.php
@@ -28,17 +28,22 @@ get_header(); ?>
 			<?php
 			endif;
 
-			/* Start the Loop */
 			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
+				get_template_part( 'template-parts/content-hero', get_post_format() );
 			endwhile;
+
+
+			/* Start the Loop */
+			// while ( have_posts() ) : the_post();
+
+				
+			// 	 * Include the Post-Format-specific template for the content.
+			// 	 * If you want to override this in a child theme, then include a file
+			// 	 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 
+			// 	get_template_part( 'template-parts/content', get_post_format() );
+
+			// endwhile;
 
 			the_posts_navigation();
 
