f16a3263f8c92ad4ba0769e7e568d494e384cd08
JocelynYH <seaweed4@live.com>
2017-06-23 17:34:39 -0400

    fixed positioning of logo on mobile navbar

--- header.php
+++ header.php
@@ -116,10 +116,10 @@
 
 	<div id="mySidenav" class="sidenav">
 	  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-	  <a href="#">About</a>
-	  <a href="#">Services</a>
-	  <a href="#">Clients</a>
-	  <a href="#">Contact</a>
+	  <a href="#">News</a>
+	  <a href="#">Arts</a>
+	  <a href="#">Living</a>
+	  <a href="#">Events</a>
 	</div>
 
 <!-- Use any element to open the sidenav -->
