77730bf17848c11ad3630cbb2301b80ed27b85c0
JocelynYH <seaweed4@live.com>
2017-06-23 18:34:43 -0400

    navbar updated

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,19 @@
 $( document ).ready(function() {
-	$(function() {
-	  	console.log("hi");
-			$(".dropdown").on("click", function() {
-				$('.dropdown-content').toggle();
-			});
-	});
+    $(function() {
+        console.log("hi");
+            $(".dropdown").on("click", function() {
+                $('.dropdown-content').toggle();
+            });
+    });
 });
-
 $( document ).ready(function() {
-	$(function() {
-	  	console.log("hello");
-			$("input.search-submit").on("click", function() {
-				$('input.search-field').toggle();
-			});
-	});
+    $(function() {
+        console.log("hello");
+            $("input.search-submit").on("click", function() {
+                $('input.search-field').toggle();
+            });
+    });
 });
-
 $(function(){
         $('ul.nav-menu li a').each(function(){
             if ($(this).prop('href') == window.location.href) {
@@ -23,14 +21,11 @@ $(function(){
             }
         });
     });
-
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
 }
-
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
