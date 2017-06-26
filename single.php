a191afb6a8e151f0c4940a074dd7e21ba9bdab94
JocelynYH <seaweed4@live.com>
2017-06-26 10:27:52 -0400

    edited navbar image styling

--- single.php
+++ single.php
@@ -15,6 +15,9 @@ get_header(); ?>
 		<?php
 		while ( have_posts() ) : the_post();
 
+			$prev = get_previous_posts_link();
+			echo $prev;
+
 			get_template_part( 'template-parts/content', get_post_format() );
 
 			the_post_navigation();
